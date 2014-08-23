934632a18c936581217fd928275e58a8062c3877
Christian Schulze <christian.schulze@mni.thm.de>
2014-08-23 19:55:03 +0200

    provided doc

--- src/ellipsis.js
+++ src/ellipsis.js
@@ -4,6 +4,11 @@ angular.module('HappyTypo.ellipse', [])
     .value(ellipse, '&hellip;')
     .factory('EllipseReplaceService', ['ellipse',
         function(ellipse) {
+            /**
+             * Replace `...` with actual ellipse
+             * @param  {String} text to replace ellipsis in
+             * @return {String}      text with replaced ellipses
+             */
             return function replaceEllipse(text) {
                 return text.replace('...', ellipse);
             };
@@ -11,6 +16,14 @@ angular.module('HappyTypo.ellipse', [])
     ])
     .filter('replaceEllipse', ['EllipseReplaceService',
         function(EllipseReplaceService) {
+            /**
+             * Filter to call EllipseReplaceService
+             *
+             * @see EllipseReplaceService
+             *
+             * @param  {String} text to replace ellipsis in
+             * @return {String}      text with replaced ellipses
+             */
             return function(text) {
                 return EllipseReplaceService.replaceEllipse(text);
             };
